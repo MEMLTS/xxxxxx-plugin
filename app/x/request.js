f72b8dbee1671a6b1d8bdc896662625bc70def0e
MapleLeaf <197728340@qq.com>
2025-04-19 23:02:51 +0800

    refactor(x): 重构请求模块以提高代码可读性和维护性
    - 移除了不必要的变量和嵌套函数
    - 使用 destructuring 获取 cookie 值
    - 简化了请求逻辑，减少了代码复杂性
    - 优化了错误处理和日志信息
    - 调整了部分变量命名以符合规范

--- app/x/request.js
+++ app/x/request.js
@@ -1,33 +1,30 @@
+/* eslint-disable camelcase */
 import { Request, getCookieValue } from '#utils'
 import { Config } from '#components'
 
 export async function getReq (url) {
-  const config = await Config.getDefOrConfig('cookie')
-  let retried = false
-
-  const baseRequest = () => {
-    const cookie = `auth_token=${config.x_auth_token};ct0=${config.x_ct0};`
-    return {
+  try {
+    const { x_auth_token, x_ct0 } = global.xxxxxx.cookie
+    const response = await Request.request({
       url,
-      cookie,
+      cookie: `auth_token=${x_auth_token};ct0=${x_ct0};`,
       headers: {
         authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
-        'x-csrf-token': config.x_ct0
+        'x-csrf-token': x_ct0
       }
-    }
-  }
-  try {
-    const response = await Request.request(baseRequest())
+    })
+
     if (response.statusCode === 403) {
-      logger.error('「xxxxxx-plugin」请求失败，正在自动更新CSRF令牌...')
+      logger.error('请求失败，正在自动更新x_ct0令牌...')
       const newCt0 = getCookieValue(response.headers['set-cookie'], 'ct0')
-      if (newCt0 && !retried) {
+
+      if (newCt0) {
         Config.modify('cookie', 'x_ct0', newCt0)
-        retried = true
-        logger.info('「xxxxxx-plugin」刷新Cookie成功，正在重试请求...')
-        return getReq(url)
+        logger.info('刷新Cookie成功，重试请求...')
+        return getReq(url) // 重试请求
       }
     }
+
     return response
   } catch (error) {
     logger.error('请求失败:', error.message)
